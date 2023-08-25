6bbfb0195dd8cbcf28e12bc975ae68f72499949f
Brian Mason <brian.kw.mason@gmail.com>
2023-08-25 15:27:24 -0400

    Finished website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import adventureaiImage from "./assets/adventureai.png";
+import kawaii from "./assets/kawaii.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Agis Companies</h1>
+      <p>Building apps for the future</p>
+      <a href="mailto:aifutureapps@gmail.com">Questions? Reach out</a>
+      <div class="container">
+        <div class="item">
+          <a
+            href="https://cool-sawine-1bfeee.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={adventureaiImage} alt="adventureai" />
+          </a>
+        </div>
+        <div class="item">
+          <a href="https://kawaiiapp.net/" target="_blank" rel="noreferrer">
+            <img src={kawaii} alt="kawaii" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
