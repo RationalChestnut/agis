ec6a7e3a68c43d34ab9863f7614fcf550f0b1e2d
Brian Mason <brian.kw.mason@gmail.com>
2024-07-17 19:55:10 -0400

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import "./App.css";
-import adventureaiImage from "./assets/adventureai.png";
-import kawaii from "./assets/kawaii.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>Agis Companies</h1>
+      <h1>Origin Apps</h1>
       <p>Building apps for the future</p>
       <a href="mailto:aifutureapps@gmail.com">Questions? Reach out</a>
-      <div class="container">
-        <div class="item">
-          <a
-            href="https://cool-sawine-1bfeee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={adventureaiImage} alt="adventureai" />
-          </a>
-        </div>
-        <div class="item">
-          <a href="https://kawaiiapp.net/" target="_blank" rel="noreferrer">
-            <img src={kawaii} alt="kawaii" />
-          </a>
-        </div>
-      </div>
     </div>
   );
 }
